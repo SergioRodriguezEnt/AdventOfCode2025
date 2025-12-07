5858477121126ba09b219167f8a1d7a22dd9768a
Sergio Rodríguez Rubio <sergiorodriguezent@gmail.com>
2025-12-07 15:37:29 +0000

    Created project

--- src/test/java/Day01ATest.java
+++ src/test/java/Day01ATest.java
@@ -1,2 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class Day01ATest {
+    @Test
+    public void test() {
+        Dial dial = new Dial();
+        assertThat(dial.position()).isEqualTo(50);
+    }
+
+    private class Dial {
+        public int position() {
+            return 50;
+        }
+    }
 }
